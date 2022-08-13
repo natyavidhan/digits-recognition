2e309c74ec3dfbe6c5024cc699d33fe8571f7ebe
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-08-13 14:23:33 +0530

    some what prediction

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 import pickle
+from PIL import Image
+import numpy as np
 
 import os
+from io import StringIO
 
 from train import train_and_save
 
@@ -28,6 +31,7 @@ class Window:
         self.font_big = pygame.font.SysFont('Arial', 30)
 
         self.canvas = pygame.Surface((350, 350))
+        self.canvas.fill((0, 0, 0))
 
     def draw(self):
         if pygame.mouse.get_pressed()[0]:
@@ -40,12 +44,32 @@ class Window:
             pos = (pos[0] - 50, pos[1] - 50)
             pygame.draw.circle(self.canvas, (0, 0, 0), pos, 5)
 
+    def predict(self):
+        data = pygame.image.tostring(self.canvas, 'RGB')
+        img = Image.frombytes('RGB', (350, 350), data)
+        img = img.resize((28, 28))
+        img = np.array(img)
+        img_arr = []
+        for y in img:
+            for x in y:
+                R, G, B = list(x)
+                L = R * 299/1000 + G * 587/1000 + B * 114/1000
+                img_arr.append(L)
+        
+        result = self.clf.predict([img_arr])
+
+        print(result)
+        return result[0]
+
 
     def run(self):
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.predict()
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.canvas, (50, 50))
             text = self.font_big.render('Draw a digit', True, (0, 0, 0))
